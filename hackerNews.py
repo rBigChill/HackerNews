55328028f13526e92f74c51fc4933432d8bd5ace
Jorge Cisneros <jcisneros@kilgore.edu>
2024-04-03 14:42:59 -0500

    removed q to quit and added enter to quit

--- hackerNews.py
+++ hackerNews.py
@@ -61,10 +61,10 @@ class HackerNews:
     def _prompt(self):
         selection = 0
 
-        while selection != 'q':
+        while selection != '':
             try:
-                selection = input("Print article (#) or (q)uit?: ")
-                if selection != 'q':
+                selection = input("Print article (#) or Enter to quit?: ")
+                if selection != '':
                     webbrowser.open(HN.frontPage[int(selection)-1].url)
                     self._clear()
                     self._printArticles()
